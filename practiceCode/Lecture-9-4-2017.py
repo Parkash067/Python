bf1f829e4d337c7e2bccd597eca5c0598fcebe61
parkash067 <pk_bscs@yahoo.com>
2017-04-09 16:15:55 +0500

    some more changes in practice code

--- practiceCode/Lecture-9-4-2017.py
+++ practiceCode/Lecture-9-4-2017.py
@@ -21,3 +21,79 @@
 #     if alphabet == 'a' or alphabet == 'i'or alphabet == 'e' or alphabet == 'o' or alphabet == 'u':
 #         count =count+1
 # print("Ther are %s vowels in string:"%count)
+
+
+# def greet():
+#     print("Hello World")
+# greet()
+#
+# def greet_user(x):
+#     print("Hello " + x)
+#
+# name = input("name????")
+# greet_user(name)
+
+# def add(num1,num2,num3):
+#     print(num1+num2+num3)
+# add(2,6,2)
+
+
+# Note : List pass by reference , whereas digits and decimals pass by value
+
+# e.g pass by value
+# def abc(x):
+#     x=90
+# x=30
+# abc(x)
+# print(x)
+#
+
+# e.g pass by reference
+# def abc(x):
+#     x[0]=50
+# x=[30,40,60,70]
+# abc(x) #pass original list
+# abc(x[:]) # pass copy of list
+# print(x)
+
+
+# def test(x,y=2):
+#     print(x,y)
+#
+# test(5,4)
+# test(x=1,y=5)
+# test(y=5,x=1)
+# test(x=6)
+# test(6)
+
+# def test(x,y):
+#     return x+y
+#
+# result = test(3,4)
+# print(result)
+# print(test(3,4))
+
+# def info(firstname,lastname):
+#     return {"first_name":firstname,"last_name":lastname}
+#
+# print(info("Parkash","Kumar"))
+
+# * is used to make tuple
+# ** is used to make dictionary
+
+# def demo(*nums):
+#     return nums
+# print(max(demo(10,20,40,15,24,25,80,100,89,120)))
+# print(sum(demo(10,20,40,15,24,25,80,100,89,120))/len(demo(10,20,40,15,24,25,80,100,89,120)))
+
+
+# def demodict(**dict):
+#     return dict
+# list1=[1,2,3]
+# list2 =[3,14,5]
+# result = demodict(list_one=list1,list_two=list2)
+# newlist=[]
+#
+# for key,value in result.items():
+#     newlist.append(value)
+# print(newlist)

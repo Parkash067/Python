0b131ef10a29061fe6b5604efc0bb177bafc9b42
parkash067 <pk_bscs@yahoo.com>
2017-04-09 14:35:56 +0500

    add changes

---
+++ practiceCode/Lecture-9-4-2017.py
@@ -0,0 +1,23 @@
+#print numbers which are divisble by 7 and 13
+# for num in range(1,100):
+#     if num%7==0 and num%13==0:
+#         print(num)
+
+# sentence = "The brown fox jumps quickly over the lazy dog"
+# words = sentence.lower().split(' ')
+# count = {}
+# for w in words:
+#     if w in count:
+#         count[w] += 1
+#     else:
+#         count[w] = 1
+# print(count)
+
+# str = "Hello World"
+# print(str[::-1])
+# print(str[::2])
+# count =0
+# for alphabet in str:
+#     if alphabet == 'a' or alphabet == 'i'or alphabet == 'e' or alphabet == 'o' or alphabet == 'u':
+#         count =count+1
+# print("Ther are %s vowels in string:"%count)
